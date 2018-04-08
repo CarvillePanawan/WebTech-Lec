22d76a67804faf90dfd3c0c6f14a978888198795
cs-13 <cs13.panawan@gmail.com>
2018-04-09 01:33:46 +0800

    Uploaded the updated website, added content and layout for both CSS and JS topics. Also fixed the overall look of the website.

---
+++ Website/JAVASCRIPT/html-wp.js
@@ -0,0 +1,37 @@
+function openTab(evt, tabtopic) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabtopic).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function sOpenTab(evt, tabtopic) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("s-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+
+    tablinks = document.getElementsByClassName("s-tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabtopic).style.display = "block";
+    evt.currentTarget.className += " active";
+}
